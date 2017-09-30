3c6925e58fa017ee7eb36691d6b9c0a53b4aeffc
AlexSashik <espiritudelnumero@gmail.com>
2017-09-30 22:51:10 +0300

    New version

--- modules/demo/static/main.php
+++ modules/demo/static/main.php
@@ -1,3 +1,3 @@
 <?php
-CORE::$CSS[] = '<link href="/css/'.CORE::$STYLE.'/main1.00.css" rel="stylesheet" type="text/css">';
+CORE::$CSS[] = '<link href="/css/'.CORE::$STYLE.'/main1.01.css" rel="stylesheet" type="text/css">';
 CORE::$JS[]  = '<script defer src="/js/'.CORE::$STYLE.'/main1.02.js"></script>';
