6965cefed41b1dad459e64bbc72107bf0e6bbad3
AlexSashik <espiritudelnumero@gmail.com>
2017-10-14 22:55:17 +0300

    New version

--- variables.php
+++ variables.php
@@ -17,8 +17,8 @@ if(isset ($_GET['route'])) {
 	} elseif (!empty($temp) && $temp[0] == 'demo' ) {
         CORE::$CONTR = CORE::$CONTR.'/demo';
         CORE::$STYLE = 'demo';
-        CORE::$META['description'] = 'Agrolux - современная сельскохозяйственная компания по продаже злаковых культур и агро оборудования';
-        CORE::$META['keywords']    = 'агрокультура, сельское хозяйство, злаковые культуры, агро товары, agrolux';
+        CORE::$META['description'] = '';
+        CORE::$META['keywords']    = '';
         CORE::$META['title']       = 'Todo-Demo';
         array_shift($temp);
     }
